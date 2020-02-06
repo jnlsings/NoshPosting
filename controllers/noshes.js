43010803219ad9049882154c2387c34fd815c453
jnlsings <jnlsings@gmail.com>
2020-02-06 12:24:13 -0600

    Add recipe data to seeds.json

--- controllers/noshes.js
+++ controllers/noshes.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const Nosh = require('../models/Nosh');
 
+const router = express.Router();
+
+// get all Noshes
+router.get('/', (req, res) => {
+  Nosh.find({})
+    .then(noshes => {
+      res.render('index', { noshes });
+    })
+    .catch(err => console.log(err));
+});
+
+module.exports = router;
--- models/Nosh.js
+++ models/Nosh.js
@@ -3,8 +3,9 @@ const mongoose = require('../db/connection');
 const NoshSchema = new mongoose.Schema({
   title: String,
   description: String,
-  ingredients: String,
-  instructions: String
+  ingredients: Array,
+  instructions: Array,
+  nutritionInfo: Array
 });
 
 const Nosh = mongoose.model('Nosh', NoshSchema);
